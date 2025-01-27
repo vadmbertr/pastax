78dcc4f9eb8ffcb8ffdcf6165a56cbbf4cb3f744
Vadim Bertrand <vadim.bertrand@univ-grenoble-alpes.fr>
2025-01-27 17:04:20 +0100

    distance_to_earth function is now grad safe, i.e. its grad does not return  for equal points

--- pastax/utils/_geo.py
+++ pastax/utils/_geo.py
@@ -27,12 +27,18 @@ def distance_on_earth(latlon1: Float[Array, "... 2"], latlon2: Float[Array, "...
     Array
         The distance between the two (array of) points in meters.
     """
+
+    def safe_for_grad_sqrt(x):
+        # grad(sqrt(x)) is not defined at x=0, here we are happy if it evaluates to 0 when x is a
+        y = jnp.sqrt(jnp.where(x != 0.0, x, 1.0))  # type: ignore
+        return jnp.where(x != 0.0, y, 0.0)
+
     lat1_rad = jnp.radians(latlon1[..., 0])
     lat2_rad = jnp.radians(latlon2[..., 0])
     d_rad = jnp.radians(latlon1 - latlon2)
 
     a = jnp.sin(d_rad[..., 0] / 2) ** 2 + jnp.cos(lat1_rad) * jnp.cos(lat2_rad) * jnp.sin(d_rad[..., 1] / 2) ** 2
-    c = 2 * jnp.atan2(jnp.sqrt(a), jnp.sqrt(1 - a))
+    c = 2 * jnp.atan2(safe_for_grad_sqrt(a), jnp.sqrt(1 - a))
     d = EARTH_RADIUS * c
 
     return d
